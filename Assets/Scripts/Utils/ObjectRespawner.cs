3a771e59cbd9855501f86f71987a06f44ae07267
fdsf <Chopk@DEfdsfSKTOP-DFVIUS0>
2019-10-16 03:10:37 -0400

    Added a new jumping flea enemy to irritate the player.

---
+++ Assets/Scripts/Utils/ObjectRespawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRespawner : MonoBehaviour {
+
+    public Transform respawnPoint;
+
+    public void OnTriggerEnter ( Collider other ) {
+        other.transform.position = respawnPoint.position;
+    }
+}
