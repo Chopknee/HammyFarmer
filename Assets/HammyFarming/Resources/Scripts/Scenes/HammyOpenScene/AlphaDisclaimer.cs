77fff8926e6cfd5400bd03d81a533283373174f7
fdsf <Chopk@DEfdsfSKTOP-DFVIUS0>
2019-11-26 23:26:27 -0500

    Final bit of organization for the night.

--- Assets/HammyFarming/Resources/Scripts/Scenes/HammyOpenScene/AlphaDisclaimer.cs
+++ Assets/HammyFarming/Resources/Scripts/Scenes/HammyOpenScene/AlphaDisclaimer.cs
@@ -1,44 +1,62 @@
-﻿using UnityEngine;
+﻿using HammyFarming.Brian.Utils.Timing;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace HammyFarming.Scenes.HammyOpenScene {
 
     public class AlphaDisclaimer : MonoBehaviour {
 
-        static bool hasShown = false;
-
         public float holdTime = 0.15f;
+        public float fadeoutTime = 0.75f;
 
-        void Start() {
-            if (hasShown) {
-                gameObject.SetActive(false);
-                hasShown = true;
-                return;
-            }
+        UnityEngine.UI.Image fillImage;
+        CanvasGroup cg;
 
-            hasShown = true;
+        Timeout holdTimeout;
+        Timeout fadeTimeout;
+
+        void Awake() {
+            fillImage = transform.Find("Panel/FillImage").GetComponent<UnityEngine.UI.Image>();
+            fillImage.fillAmount = 1;
+
+            cg = GetComponent<CanvasGroup>();
+
+            holdTimeout = new Timeout(holdTime, false);
+            fadeTimeout = new Timeout(fadeoutTime, false);
+
             HammyFarming.Brian.GameManagement.PlayerInput.ControlMaster.InputDevice.MainMenuStart.Disable();
         }
-
-        void AnyActionPerformed(InputAction.CallbackContext context) {
-            gameObject.SetActive(false);
-        }
-
-        float t = 0.0f;
 
         private void Update () {
             float btn1 = HammyFarming.Brian.GameManagement.PlayerInput.ControlMaster.InputDevice.GamepadAnyButton.ReadValue<float>();
             float btn2 = HammyFarming.Brian.GameManagement.PlayerInput.ControlMaster.InputDevice.KeyboardAnyButton.ReadValue<float>();
 
-            if (btn1 > 0.0f || btn2 > 0.0f) {
-                t+= Time.deltaTime;
-                if (t > holdTime) {
-                    //Switch to the next scene
-                    gameObject.SetActive(false);
-                    HammyFarming.Brian.GameManagement.PlayerInput.ControlMaster.InputDevice.MainMenuStart.Enable();
-                }
+            //For holding the button
+            if (btn1 > 0.0f || btn2 > 0.0f && !holdTimeout.running) {
+                holdTimeout.Start();
             } else {
-                t = 0.0f;
+                if (!fadeTimeout.running) {
+                    holdTimeout.Reset();
+                    fillImage.fillAmount = 1.0f;
+                }
+            }
+
+            if (holdTimeout.running) {
+                fillImage.fillAmount = 1.0f - holdTimeout.NormalizedTime;
+            }
+
+            if (holdTimeout.Tick(Time.deltaTime)) {
+                fadeTimeout.Start();
+            }
+
+            //For fading the screen out.
+            if (fadeTimeout.running) {
+                cg.alpha = 1 - fadeTimeout.NormalizedTime;
+            }
+
+            if (fadeTimeout.Tick(Time.deltaTime)) {
+                gameObject.SetActive(false);
+                HammyFarming.Brian.GameManagement.PlayerInput.ControlMaster.InputDevice.MainMenuStart.Enable();
             }
         }
     }
